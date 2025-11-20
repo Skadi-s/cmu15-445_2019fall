67494bca7a257349b4689e586cd0d5df98c49a17
TreeSharkMac <1632504075@qq.com>
2025-11-20 16:45:51 +0800

    storage/disk: implement DiskScheduler worker, Schedule and documentation

--- src/storage/disk/disk_scheduler.cpp
+++ src/storage/disk/disk_scheduler.cpp
@@ -18,9 +18,9 @@
 namespace bustub {
 
 DiskScheduler::DiskScheduler(DiskManager *disk_manager) : disk_manager_(disk_manager) {
-  UNIMPLEMENTED("TODO(P1): Add implementation.");
-  // Spawn the background thread
-  background_thread_.emplace([&] { StartWorkerThread(); });
+  // Spawn the background thread which will process requests from the channel.
+  // The worker will run until the destructor enqueues a `std::nullopt` sentinel.
+  background_thread_.emplace([this] { StartWorkerThread(); });
 }
 
 DiskScheduler::~DiskScheduler() {
@@ -38,7 +38,15 @@ DiskScheduler::~DiskScheduler() {
  *
  * @param requests The requests to be scheduled.
  */
-void DiskScheduler::Schedule(std::vector<DiskRequest> &requests) {}
+void DiskScheduler::Schedule(std::vector<DiskRequest> &requests) {
+  // Non-blocking enqueue of requests.
+  // Each DiskRequest may optionally contain a promise in `callback_` that the
+  // caller can wait on to observe completion. We move each request into the
+  // shared channel so the background worker can pick it up and execute it.
+  for (auto &req : requests) {
+    request_queue_.Put(std::make_optional<DiskRequest>(std::move(req)));
+  }
+}
 
 /**
  * TODO(P1): Add implementation
@@ -48,6 +56,38 @@ void DiskScheduler::Schedule(std::vector<DiskRequest> &requests) {}
  * The background thread needs to process requests while the DiskScheduler exists, i.e., this function should not
  * return until ~DiskScheduler() is called. At that point you need to make sure that the function does return.
  */
-void DiskScheduler::StartWorkerThread() {}
+void DiskScheduler::StartWorkerThread() {
+  // Background worker loop: consume DiskRequest objects from the request_queue_
+  // Channel semantics: Get() blocks until an element is available. When the
+  // destructor wants the worker to exit it will push a `std::nullopt` sentinel.
+  while (true) {
+    auto opt = request_queue_.Get();
+    if (!opt.has_value()) {
+      // Shutdown signal received; exit the worker loop.
+      break;
+    }
+
+    // Take ownership of the request and process it.
+    DiskRequest req = std::move(opt.value());
+
+    // Execute the I/O using the DiskManager. The DiskManager implementations
+    // are expected to be synchronous (blocking) calls that complete the I/O
+    // before returning. This keeps the DiskScheduler simple: scheduling is
+    // asynchronous, but completion can be observed via the optional promise.
+    if (req.is_write_) {
+      disk_manager_->WritePage(req.page_id_, req.data_);
+    } else {
+      disk_manager_->ReadPage(req.page_id_, req.data_);
+    }
+
+    // Fulfill the promise if the caller provided one. Use try/catch because
+    // the future side may have been discarded by the caller.
+    try {
+      req.callback_.set_value(true);
+    } catch (...) {
+      // Ignore errors setting the promise (e.g., no consumer).
+    }
+  }
+}
 
 }  // namespace bustub
